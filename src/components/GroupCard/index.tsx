bf273111e4c59e9b0483464260719060a136873e
“Thiago <thiago.osamu@outlook.com>
2024-07-06 03:31:19 -0300

    add list

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableHighlightProps } from "react-native";
+import { Container, Icon, Title } from "./styles";
+
+  type Props = TouchableHighlightProps & {
+    title: string;
+  }
+
+export function GroupCard({ title, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Icon />
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,28 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
   return (
     <Container>
       <Header />
+
       <Highlight 
         title='Turmas'
         subtitle='Jogue com a sua turma'
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
       />
     </Container>
   );
