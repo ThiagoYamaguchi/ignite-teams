2f8c0d537cc55baddaecd3e8bba8242c0d4df8f4
“Thiago <thiago.osamu@outlook.com>
2024-07-14 20:03:05 -0300

    refatorando css

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,11 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -23,7 +25,15 @@ export default function Groups() {
         renderItem={({ item }) => (
           <GroupCard title={item} />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
+          />
+        )}
       />
+
+      <Button title='Criar nova turma'/>
     </Container>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,29 @@
+import { Header } from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header ShowBackButton />
+
+      <Content>
+        <Icon />
+        
+        <Highlight 
+          title='Nova turma'
+          subtitle='Crie a turma para adicionar as pessoas'
+        />
+
+        <Input placeholder="Nome da turma"/>
+
+        <Button 
+          title='Criar' 
+          style={{ marginTop: 20 }}
+        />
+      </Content>
+    </Container>
+  );
+}
