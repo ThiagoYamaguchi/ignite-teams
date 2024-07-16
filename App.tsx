0ede381f8ef231bc31348a3d8614e08d7bbcc649
“Thiago <thiago.osamu@outlook.com>
2024-07-16 01:45:50 -0300

    finalizando a lista de players

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
-import Groups from './src/screens/Groups';
+import { Players } from './src/screens/Players';
 import theme from './src/theme';
 
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
