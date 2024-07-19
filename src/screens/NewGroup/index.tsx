a23e5bbb83f51feb90b2d9b1584521b2ecfd925a
“Thiago <thiago.osamu@outlook.com>
2024-07-19 00:49:36 -0300

    finalizando navigation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
+
   const navigation = useNavigation();
   
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket'});
+    navigation.navigate('players', { group });
   }
 
   return (
@@ -24,7 +27,10 @@ export function NewGroup() {
           subtitle='Crie a turma para adicionar as pessoas'
         />
 
-        <Input placeholder="Nome da turma"/>
+        <Input 
+          placeholder="Nome da turma"
+          onChangeText={setGroup}
+        />
 
         <Button 
           title='Criar' 
