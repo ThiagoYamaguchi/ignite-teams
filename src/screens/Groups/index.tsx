304a6a830593aba3e88905207336d51219d79931
“Thiago <thiago.osamu@outlook.com>
2024-07-24 02:00:24 -0300

    Criando asyncstorage para o player

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -27,6 +27,10 @@ export default function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group });
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups();
   }, []));
@@ -44,7 +48,10 @@ export default function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <GroupCard title={item} />
+          <GroupCard 
+            title={item} 
+            onPress={() => handleOpenGroup(item)}
+          />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1}}
         ListEmptyComponent={() => (
